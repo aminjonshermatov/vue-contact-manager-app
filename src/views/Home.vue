f9a4bbe295d41ed4867089690fe9b7a29c41b61e
Aminjon <aminjonshermatov@gmail.com>
2020-10-14 14:42:28 +0500

    orig works

---
+++ src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h2>Home page</h2>
+
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam, est molestias et repellat consequuntur quaerat explicabo nemo sed deleniti ab error voluptate ratione qui voluptatibus veritatis tempore, ex commodi nulla?</p>
+
+        <router-link class="router" to="/todos">Todos</router-link>
+    </div>
+</template>
+
+<style scoped>
+    .router {
+        color: #000;
+    }
+</style>
